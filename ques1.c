51737e3467c470d3d487ad8c1617d04ed6ae74ed
tanisha1120 <tanishasingh.cse23@kgikanpur.in>
2024-05-29 19:18:33 +0530

    Create ques1.c

---
+++ ques1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a,b = 0;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &a);
+
+    for (int i = 1; i <= n; i++) {
+        b += i;
+    }
+
+    printf("Sum of the numbers is: %d\n",b);
+
+    return 0;
+}
